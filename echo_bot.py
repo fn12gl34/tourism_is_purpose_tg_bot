7406e4b890cda6f619940a2a00046cecc84e5209
Dmitry <dmitry.kuzmin@inten.to>
2022-09-24 20:36:19 +0400

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,21 +1,45 @@
 import os
 import telebot
-from flask import Flask, request
+import flask
 
 TG_BOT_APIKEY = os.getenv('TG_BOT_APIKEY') 
 
+WEBHOOK_HOST = 'https://tourism-is-purpose-tg-bot.herokuapp.com/'
+WEBHOOK_PORT = os.environ.get('PORT', 80)  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0' 
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (TG_BOT_APIKEY)
+
 bot = telebot.TeleBot(TG_BOT_APIKEY, parse_mode=None)
-server = Flask('tourism_is_purpose_tg_bot')
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return ''
+
+
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
 
 
 @bot.message_handler(content_types=['new_chat_members'])
 def remove_info(message):
     bot.delete_message(message.chat.id, message.message_id)
+
     
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://tourism-is-purpose-tg-bot.herokuapp.com/') # этот url нужно заменить на url вашего Хероку приложения
-    return '?', 200
-    
-server.run(host='0.0.0.0', port=os.environ.get('PORT', 80))
+bot.remove_webhook()
+
+time.sleep(0.1)
+
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+
+app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT)
