99f3593bf9dd6f3711b8a68b0231d7d446e95a92
Dmitry <dmitry.kuzmin@inten.to>
2022-09-24 19:56:36 +0400

    Add files via upload

---
+++ echo_bot.py
@@ -0,0 +1,18 @@
+import os
+import telebot
+
+TG_BOT_APIKEY = os.getenv('TG_BOT_APIKEY') 
+
+bot = telebot.TeleBot(TG_BOT_APIKEY, parse_mode=None)
+
+@bot.message_handler(content_types=['new_chat_members'])
+def remove_info(message):
+    bot.delete_message(message.chat.id, message.message_id)
+
+@bot.message_handler(content_types=['new_chat_members'])
+def remove_info(message):
+    bot.delete_message(message.chat.id, message.message_id)
+
+
+bot.infinity_polling()
+
