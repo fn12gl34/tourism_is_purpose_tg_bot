9c84b55131f293a692ab49f0bec8ccdd89b6f7e3
Dmitry <dmitry.kuzmin@inten.to>
2022-09-24 21:51:59 +0400

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,35 +1,26 @@
 import os
-import time
+
+from flask import Flask, request
 import telebot
-import flask
 
-TG_BOT_APIKEY = os.getenv('TG_BOT_APIKEY') 
-
-WEBHOOK_HOST = 'tourism-is-purpose-tg-bot.herokuapp.com/'
-WEBHOOK_PORT = os.environ.get('PORT', 80)  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '0.0.0.0' 
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (TG_BOT_APIKEY)
-
-bot = telebot.TeleBot(TG_BOT_APIKEY, parse_mode=None)
-app = flask.Flask(__name__)
+TOKEN = os.getenv('TG_BOT_APIKEY') 
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'HEAD'])
-def index():
-    return ''
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
 
 
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+@server.route('/')
 def webhook():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_string = flask.request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        flask.abort(403)
+    bot.remove_webhook()
+    bot.set_webhook(url='https://tourism-is-purpose-tg-bot.herokuapp.com/' + TOKEN)
+    return '!', 200
 
 
 @bot.message_handler(content_types=['new_chat_members'])
@@ -37,10 +28,4 @@ def remove_info(message):
     bot.delete_message(message.chat.id, message.message_id)
 
     
-bot.remove_webhook()
-
-time.sleep(0.1)
-
-bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
-
-app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT)
+server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
